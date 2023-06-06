e462b6a67a4a92572f782b055c3c4ad144cac709
junaidh <junaidh.2307@gmail.com>
2023-06-06 15:14:57 +0530

    curved navigation bar created

--- lib/landingPage.dart
+++ lib/landingPage.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, override_on_non_overriding_member, unused_field
 
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:shoffz/AccountPage.dart';
 import 'package:shoffz/HomePage.dart';
@@ -25,6 +26,25 @@ class _LandingPageState extends State<LandingPage> {
 
   String? _appbartext = "Shoffz";
 
+  final navigationitems = <Widget>[
+    Icon(
+      Icons.home,
+      color: Colors.black,
+    ),
+    Icon(
+      Icons.add_circle_outline_rounded,
+      color: Colors.black,
+    ),
+    Icon(
+      Icons.favorite_outline_outlined,
+      color: Colors.black,
+    ),
+    Icon(
+      Icons.person_outline_rounded,
+      color: Colors.black,
+    ),
+  ];
+
   @override
   void _onItemTapped(int index) {
     setState(() {
@@ -35,6 +55,7 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: Text("shoffz"),
@@ -53,32 +74,14 @@ class _LandingPageState extends State<LandingPage> {
       body: Center(
         child: _WidgetOption.elementAt(_SelectedIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedLabelStyle: TextStyle(color: Colors.white),
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: ("Home"),
-              backgroundColor: Colors.green),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.add_circle_outline_rounded),
-              label: ("Rent"),
-              backgroundColor: Colors.green),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_outline_outlined),
-              label: ("My Fav"),
-              backgroundColor: Colors.green),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline_rounded),
-              label: ("Account"),
-              backgroundColor: Colors.green),
-        ],
-        type: BottomNavigationBarType.shifting,
-        currentIndex: _SelectedIndex,
-        selectedItemColor: Colors.black,
-        iconSize: 20,
+      bottomNavigationBar: CurvedNavigationBar(
+        animationCurve: Curves.easeIn,
+        color: Colors.green,
+        buttonBackgroundColor: Colors.white,
+        backgroundColor: Colors.transparent,
+        items: navigationitems,
+        index: _SelectedIndex,
         onTap: _onItemTapped,
-        elevation: 10,
       ),
     );
   }
